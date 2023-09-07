ba474555b8a978a7accb264a3c1c77bb30128de1
M0Coding <mohamedrejeb445@gmail.com>
2023-09-07 11:56:47 +0100

    Update signing

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -15,8 +15,11 @@ nexusPublishing {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
 //            stagingProfileId.set(System.getenv("OSSRH_STAGING_PROFILE_ID"))
-            username.set(project.findProperty("sonatypeUsername")?.toString()?.replaceFirst("'", "")?.replaceFirst("'", ""))
-            password.set(project.findProperty("sonatypePassword")?.toString()?.replaceFirst("'", "")?.replaceFirst("'", ""))
+//            username.set(project.findProperty("sonatypeUsername")?.toString()?.replaceFirst("'", "")?.replaceFirst("'", ""))
+//            password.set(project.findProperty("sonatypePassword")?.toString()?.replaceFirst("'", "")?.replaceFirst("'", ""))
+            stagingProfileId.set(System.getenv("OSSRH_STAGING_PROFILE_ID"))
+            username.set(System.getenv("OSSRH_USERNAME"))
+            password.set(System.getenv("OSSRH_PASSWORD"))
         }
     }
 }

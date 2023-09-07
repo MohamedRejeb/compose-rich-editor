53bf6e63ffbf51a54bd1e749053d0f36e80607b3
M0Coding <mohamedrejeb445@gmail.com>
2023-09-07 11:12:13 +0100

    Update signing

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -11,14 +11,15 @@ nexusPublishing {
     // Configure maven central repository
     // https://github.com/gradle-nexus/publish-plugin#publishing-to-maven-central-via-sonatype-ossrh
     repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            stagingProfileId.set(System.getenv("OSSRH_STAGING_PROFILE_ID"))
-            println("OSSRH_USERNAME")
-            println(System.getenv("OSSRH_USERNAME"))
-            username.set(System.getenv("OSSRH_USERNAME"))
-            password.set(System.getenv("OSSRH_PASSWORD"))
-        }
+        sonatype()
+//        sonatype {
+//            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+//            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+//            stagingProfileId.set(System.getenv("OSSRH_STAGING_PROFILE_ID"))
+//            println("OSSRH_USERNAME")
+//            println(System.getenv("OSSRH_USERNAME"))
+//            username.set(System.getenv("OSSRH_USERNAME"))
+//            password.set(System.getenv("OSSRH_PASSWORD"))
+//        }
     }
 }

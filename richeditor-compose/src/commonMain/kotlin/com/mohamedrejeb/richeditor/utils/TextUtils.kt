cb847559cfd267d62fba72b74bce9301a6d49ba8
MohamedRejeb <mohamedrejeb445@gmail.com>
2024-08-20 20:14:56 +0100

    Add missing imports

--- richeditor-compose/src/commonMain/kotlin/com/mohamedrejeb/richeditor/utils/TextUtils.kt
+++ richeditor-compose/src/commonMain/kotlin/com/mohamedrejeb/richeditor/utils/TextUtils.kt
@@ -2,16 +2,18 @@ package com.mohamedrejeb.richeditor.utils
 
 import com.mohamedrejeb.richeditor.annotation.ExperimentalRichTextApi
 import com.mohamedrejeb.richeditor.model.RichSpan
-import com.mohamedrejeb.richeditor.model.RichTextState
 import com.mohamedrejeb.richeditor.paragraph.RichParagraph
 import com.mohamedrejeb.richeditor.paragraph.type.ParagraphType.Companion.startText
+import kotlin.text.buildString
 import kotlin.text.appendLine
+import kotlin.text.StringBuilder
 
 @OptIn(ExperimentalRichTextApi::class)
 internal fun toText(richParagraphList: List<RichParagraph>): String =
     buildString {
         richParagraphList.fastForEachIndexed { i, richParagraphStyle ->
             append(richParagraphStyle.type.startText)
+
             append(
                 richSpanList = richParagraphStyle.children,
             )

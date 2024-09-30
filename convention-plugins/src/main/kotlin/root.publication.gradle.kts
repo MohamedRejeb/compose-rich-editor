cdd07792586a12bf9fb5602ec8ba360eaab254b2
MohamedRejeb <mohamedrejeb445@gmail.com>
2024-09-30 20:28:03 +0100

    Update version to 1.0.0-rc08

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "com.mohamedrejeb.richeditor"
-    version = System.getenv("VERSION") ?: "1.0.0-rc07"
+    version = System.getenv("VERSION") ?: "1.0.0-rc08"
 }
 
 nexusPublishing {

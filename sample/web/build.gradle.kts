c977cf8681b31840858db5189067f99f3a8deb6f
MohamedRejeb <mohamedrejeb445@gmail.com>
2024-11-10 13:31:30 +0100

    Improve gradle setup

--- sample/web/build.gradle.kts
+++ sample/web/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)

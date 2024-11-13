dde84e5139f0abe9ee9a9ab0464f6164395b1492
Adam Brown <adamwbrown@gmail.com>
2024-11-12 18:36:38 -0800

    It compiles again

--- richeditor-compose/src/commonMain/kotlin/com/mohamedrejeb/richeditor/model/RichTextState.kt
+++ richeditor-compose/src/commonMain/kotlin/com/mohamedrejeb/richeditor/model/RichTextState.kt
@@ -76,7 +76,7 @@ public class RichTextState internal constructor(
 
     internal var singleParagraphMode by mutableStateOf(false)
 
-    var textLayoutResult: TextLayoutResult? by mutableStateOf(null)
+    public var textLayoutResult: TextLayoutResult? by mutableStateOf(null)
         private set
 
     private var lastPressPosition: Offset? by mutableStateOf(null)
@@ -2731,7 +2731,7 @@ public class RichTextState internal constructor(
         return richSpan
     }
 
-    fun getRichSpanByOffset(offset: Offset): RichSpan? {
+    public fun getRichSpanByOffset(offset: Offset): RichSpan? {
         this.textLayoutResult?.let { textLayoutResult ->
             val position = textLayoutResult.getOffsetForPosition(offset)
             return getRichSpanByTextIndex(position, true)

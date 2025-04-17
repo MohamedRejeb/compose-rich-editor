7c4ae44ecbd290b00f08c040acf4558004268def
Devin D <58495296+DevinDuricka@users.noreply.github.com>
2025-04-17 14:09:55 -0700

    Following the pattern of the ElementsSpanStyle, I created the ElementsParagraphStyle.kt to hold a reference to the ParagraphStyle (if applicable)

---
+++ richeditor-compose/src/commonMain/kotlin/com/mohamedrejeb/richeditor/parser/utils/ElementsParagraphStyle.kt
@@ -0,0 +1,8 @@
+package com.mohamedrejeb.richeditor.parser.utils
+
+internal val H1ParagraphStyle = HeadingParagraphStyle.H1.getParagraphStyle()
+internal val H2ParagraphStyle = HeadingParagraphStyle.H2.getParagraphStyle()
+internal val H3ParagraphStyle = HeadingParagraphStyle.H3.getParagraphStyle()
+internal val H4ParagraphStyle = HeadingParagraphStyle.H4.getParagraphStyle()
+internal val H5ParagraphStyle = HeadingParagraphStyle.H5.getParagraphStyle()
+internal val H6ParagraphStyle = HeadingParagraphStyle.H6.getParagraphStyle()

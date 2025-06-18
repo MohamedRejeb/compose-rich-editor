42f5675ab3d83f93401f8b179e4105b8d13af802
MohamedRejeb <mohamedrejeb445@gmail.com>
2025-06-18 21:31:20 +0100

    Update version to 1.0.0-rc13

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "com.mohamedrejeb.richeditor"
-    version = System.getenv("VERSION") ?: "1.0.0-rc12"
+    version = System.getenv("VERSION") ?: "1.0.0-rc13"
 }
 
 nexusPublishing {
